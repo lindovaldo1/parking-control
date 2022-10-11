fff5dac763848ad7e21dc16822c4fb1d142767f7
lindovaldo1 <lindovaldo.leao@estudante.iftm.edu.br>
2022-10-11 05:31:35 -0300

    FEAT: create class 'ParkingSpotService' and injection point parkingSoptRepository

---
+++ src/main/java/com/michelebrito/parkingcontrol/services/ParkingSpotService.java
@@ -0,0 +1,16 @@
+package com.michelebrito.parkingcontrol.services;
+
+import com.michelebrito.parkingcontrol.repositories.ParkingSpotRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ParkingSpotService {
+
+    final ParkingSpotRepository parkingSpotRepository;
+
+    public ParkingSpotService(ParkingSpotRepository parkingSpotRepository){
+        this.parkingSpotRepository = parkingSpotRepository;
+    }
+
+
+}
