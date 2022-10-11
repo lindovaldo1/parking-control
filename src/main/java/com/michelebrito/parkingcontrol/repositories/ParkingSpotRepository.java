0b9cbcddedea11ad37d04f2742ed6c627f9d4a91
lindovaldo1 <lindovaldo.leao@estudante.iftm.edu.br>
2022-10-11 05:25:37 -0300

    feat: create interface 'ParkingSportRepository' and extends JpaRepository

---
+++ src/main/java/com/michelebrito/parkingcontrol/repositories/ParkingSpotRepository.java
@@ -0,0 +1,10 @@
+package com.michelebrito.parkingcontrol.repositories;
+
+import com.michelebrito.parkingcontrol.models.ParkingSpotModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {}
